d1a70af7115ab16c9c1a50ac571f246e2d5caf94
Spaniard Dev <spaniarddeveloper@gmail.com>
2018-06-29 14:24:36 +0200

    Finished project

--- README.md
+++ README.md
@@ -1,57 +1,10 @@
-# Easy Webpack
+# CodePen Challenge on Menus /June 2018/
 
-This is an basic and easy to use template/recipe made for Webpack.
+This is a fun side project for a CodePen Challenge on the last week of June 2018.
 
-If you want to have a **live server watching for your changes** working with a transpiler that **transforms your modern Javascript into legacy Javascript code** and a **CSS prefixer** so you don't need to code for each and every browser in the market, this template might be for you.
+Participants were presented with an HTML menu structure and they were required to make anything with it. So I decided to use SVG animation with SnapSVG for the morphing.
 
-It's main purpose is to help beginners speed up their development workflow while learning HTML, CSS and Javascript without loosing their minds trying to set up Webpack. Even if Webpack nowadays is fairly easy to use for basic stuff, beginners get overwhelmed by this kind of tools quite often.
-
-Want to try this template? Beginner or not, you're more than welcome.
-<br />
-
-##### BEFORE USING IT
-
-Before you start using this template, you will need to have installed `Node.js` and `NPM`. You can [download both from here](https://nodejs.org/en/download/).
-
-Of course, you will need a browser, a terminal/command line and a code editor.
-<br/>
-
-##### USAGE
-
-To use this template, just make a new folder and `clone` this repo:
-
-```
-mkdir your-new-project
-cd your-new-project
-git clone https://github.com/yagoestevez/easy-webpack.git .
-```
-
-After the repo has been cloned successfully, install the dependencies, like this:
-
-```
-npm install
-```
-
-Now you can start using it. Just type:
-
-```
-npm start
-```
-
-A server will start and a new browser window will open with boilerplate code. Just remove the boilerplate code and type your own; you will see the result of your code updated after every changes made. 
-
-Now, start building your next big thing!
-
-##### TODO
-
-- Add Sass and Pug to the template.
-- Optimize code for production.
-
-##### CONTRIBUTING
-
-If you feel like giving a hand to this project you can do so by implementing new stuff (nothing pro, remember it's Easy Webpack hehe...). You can also help by testing it and giving feedback so we all can figure out how to make it better.
-
-###### Anyways, thank you for reading!
+Challenge is over!
 
 ---
 
--- src/main.js
+++ src/main.js
@@ -2,22 +2,204 @@
  * Brought to you by Yago Estévez. https://twitter.com/yagoestevez
  */
 require( "babel-runtime/regenerator" );
-require( './index.html'              );
-require( './main.css'                );
+import './index.html';
+import './main.css';
+import './svg.css';
+import Sketch from './svg.js';
 
-/* BOILERPLATE CODE. REMOVE IT TO START BUILDING YOUR NEXT THING */
-let rotate = true;
-const rotateLogo = ( ) => {
-  const animation = setInterval( ( ) => {
-    document.querySelector( '.cube__outer' )
-      .style.transform = `scale3d(1, 1, 1) rotateX(0deg) rotateY(${rotate?0:450}deg) rotateZ(0deg)`;
-    document.querySelector( '.cube__inner' )
-      .style.transform = `scale3d(.5, .5, .5) rotateX(0deg) rotateY(-${rotate?0:450}deg) rotateZ(0deg)`;
-    rotate = !rotate;
-  }, 4000 );
+/******************************************************************************
+  ADDING THE SVG AND ANIMATION LIBRARY TO THE DOM.
+ ******************************************************************************/
+// Add the SVG container to the HTML.
+const nav = document.querySelector( '.menu' );
+nav.insertAdjacentHTML( 'afterend', Sketch );
+
+// Define the SVG element to be animated with SnapSVG.
+const svg = document.getElementById( 'SVG' );
+Snap( svg );
+
+// Insert copy.
+const link     = document.createElement( 'a' );
+const linkText = document.createTextNode( 'Yago Estévez' );
+link.id        = 'author';
+link.title     = "Yago Estévez on Twitter";
+link.href      = "https://twitter.com/yagoestevez";
+link.target    = "_blank";
+document.body.appendChild( link );
+link.appendChild( linkText );
+
+/******************************************************************************
+  VARIABLE DEFINITIONS FOR THE ANIMATION.
+ ******************************************************************************/
+// Define the constants for speed and slight movements of the character.
+const speed             = 300;
+const lowPose           = 'translate(0,10)';
+const lowerPose         = 'translate(0,20)';
+const lowestPose        = 'translate(0,30)';
+const lookRightPose     = 'translate(-10,0)';
+const lookMoreRightPose = 'translate(-20,0)';
+const lookLeftPose      = 'translate(10,0)';
+const lookMoreLeftPose  = 'translate(20,0)';
+const resetPose         = 'translate(0,0)';
+
+// Store character poses for animation.
+const BodyInitialPose   = Snap.select( '#Jorge-Body-Pose1'       );
+const BodyEndingPose    = Snap.select( '#Jorge-Body-Pose2'       );
+const BodyInitPosePath  = BodyInitialPose.node.getAttribute( 'd' );
+const BodyEndPosePath   = BodyEndingPose.node.getAttribute(  'd' );
+
+// Store character pieces individually.
+const Head              = Snap.select( '#Jorge-Head'             );
+const HeadDown          = Snap.select( '#Jorge-Head-Down'        );
+const Eyebrows          = Snap.select( '#Jorge-Eyebrows'         );
+const Eyes              = Snap.select( '#Jorge-Eyes'             );
+const Cheeks            = Snap.select( '#Jorge-Cheeks'           );
+const Ears              = Snap.select( '#Jorge-Ears'             );
+const Mouth             = Snap.select( '#Jorge-Mouth'            );
+const MouthSmile        = Snap.select( '#Jorge-Mouth-BigSmile'   );
+const MouthOpen         = Snap.select( '#Jorge-Mouth-SmileOpen'  );
+const Nose              = Snap.select( '#Jorge-Nose'             );
+const HeadPath          = Head.node.getAttribute(         'd'    );
+const HeadDownPath      = HeadDown.node.getAttribute(     'd'    );
+const MouthPath         = Mouth.node.getAttribute(        'd'    );
+const MouthSmilePath    = MouthSmile.node.getAttribute(   'd'    );
+const MouthOpenPath     = MouthOpen.node.getAttribute(    'd'    );
+
+/******************************************************************************
+  ANIMATION FUNCTIONS.
+ ******************************************************************************/
+// Define the animation when showing sub-menus.
+const enterMenuPose = ( ) => {
+  BodyInitialPose.animate( { d: BodyEndPosePath }, speed, mina.backout, null );
+  Head.animate( { d: HeadDownPath }, speed, mina.backout, null );
+  Eyebrows.animate( { transform: lowPose }, speed, mina.backout, null );
+  Eyes.animate( { transform: lowestPose }, speed, mina.backout, null );
+  Cheeks.animate( { transform: lowestPose}, speed, mina.backout, null );
+  Ears.animate( { transform: lowerPose}, speed, mina.backout, null );
+  Mouth.animate( { d: MouthOpenPath }, speed, mina.backout, null );
+  Nose.animate( { transform: lowPose }, speed, mina.backout, null );
 }
-window.addEventListener( 'load', ( ) => {
-  setTimeout( ( ) => {
-    rotateLogo( );
-  }, 500 );
+
+// Define the animation when looking at About menu.
+const enterLookRight = ( ) => {
+  Head.animate( { transform: 'matrix(1,0,0,1,5,0)' }, speed, mina.backout, null );
+  Ears.animate( { transform: 'matrix(1,0,0,1,8,0)' }, speed, mina.backout, null );
+  Eyes.animate( { transform: lookMoreRightPose }, speed, mina.backout, null );
+  Cheeks.animate( { transform: lookRightPose}, speed, mina.backout, null );
+  Mouth.animate( { d: MouthSmilePath }, speed, mina.backout, null );
+}
+
+// Define the animation when looking at Contact menu.
+const enterLookLeft = ( ) => {
+  Head.animate( { transform: 'matrix(1,0,0,1,-5,0)' }, speed, mina.backout, null );
+  Ears.animate( { transform: 'matrix(1,0,0,1,-8,0)' }, speed, mina.backout, null );
+  Eyes.animate( { transform: lookMoreLeftPose }, speed, mina.backout, null );
+  Cheeks.animate( { transform: lookLeftPose}, speed, mina.backout, null );
+  Mouth.animate( { d: MouthSmilePath }, speed, mina.backout, null );
+}
+
+// Define the animation when looking at Home menu.
+const enterLookDown = ( ) => {
+  Head.animate( { transform: lowerPose }, speed, mina.backout, null );
+  Ears.animate( { transform: lowerPose }, speed, mina.backout, null );
+  Eyes.animate( { transform: lowestPose }, speed, mina.backout, null );
+  Eyebrows.animate( { transform: lowerPose }, speed, mina.backout, null );
+  Cheeks.animate( { transform: lowestPose}, speed, mina.backout, null );
+  Mouth.animate( { d: MouthSmilePath, transform: lowerPose }, speed, mina.backout, null );
+  Nose.animate( { transform: lowerPose }, speed, mina.backout, null );
+}
+
+// Define the animation when hiding sub-menus.
+const leavePose = ( ) => {  
+  BodyInitialPose.animate( { d: BodyInitPosePath }, speed, mina.backout, null );
+  Head.animate( { d: HeadPath, transform: resetPose }, speed, mina.backout, null );
+  Eyebrows.animate( { transform: resetPose }, speed, mina.backout, null );
+  Eyes.animate( { transform: resetPose }, speed, mina.backout, null );
+  Cheeks.animate( { transform: resetPose }, speed, mina.backout, null );
+  Ears.animate( { transform: resetPose }, speed, mina.backout, null );
+  Mouth.animate( { d: MouthPath, transform: resetPose }, speed, mina.backout, null );
+  Nose.animate( { transform: resetPose }, speed, mina.backout, null );
+}
+
+// Reset the character's pose and submenus so it can start a new pose.
+const resetToNewPose = ( ) => {
+  onSubMenu = false;
+  leavePose( );
+  WidgetsSubMenuGroup.classList.remove( 'showSubMenu' );
+  KabobsSubMenuGroup.classList.remove( 'showSubMenu' );
+}
+
+
+/******************************************************************************
+  LISTENERS.
+ ******************************************************************************/
+let onSubMenu = false;
+
+// Listeners for the About Menu Item.
+const AboutMenuItem       = document.querySelector( '#About' );
+AboutMenuItem.addEventListener( 'mouseover', ( ) => {
+  resetToNewPose( );
+  enterLookRight( );
+} );
+AboutMenuItem.addEventListener( 'mouseout', ( ) => {
+  leavePose( );
+} );
+
+// Listeners for the Contact Menu Item.
+const ContactMenuItem     = document.querySelector( '#Contact' );
+ContactMenuItem.addEventListener( 'mouseover', ( ) => {
+  resetToNewPose( );
+  enterLookLeft( );
+} );
+ContactMenuItem.addEventListener( 'mouseout', ( ) => {
+  leavePose( );
+} );
+
+// Listeners for the Home Menu Item.
+const HomeMenuItem        = document.querySelector( '#Home' );
+HomeMenuItem.addEventListener( 'mouseover', ( ) => {
+  resetToNewPose( );
+  enterLookDown( );
+} );
+HomeMenuItem.addEventListener( 'mouseout', ( ) => {
+  leavePose( );
+} );
+
+
+// Listeners for the Widgets Menu Item.
+const WidgetsMenuItem     = document.querySelector( '#Widgets' );
+const WidgetsSubMenuGroup = document.querySelector( '#Widgets-SubMenu' );
+WidgetsMenuItem.addEventListener( 'mouseover', ( ) => {
+  KabobsSubMenuGroup.classList.remove( 'showSubMenu' );
+  onSubMenu = true;
+  enterMenuPose( );
+  WidgetsSubMenuGroup.classList.add( 'showSubMenu' );
+} );
+WidgetsMenuItem.addEventListener( 'mouseout', ( ) => {
+  if ( !onSubMenu ) {
+    leavePose( );
+    WidgetsSubMenuGroup.classList.remove( 'showSubMenu' );
+  }
+} );
+
+// Listeners for the Kabobs Menu Item.
+const KabobsMenuItem      = document.querySelector( '#Kabobs' );
+const KabobsSubMenuGroup  = document.querySelector( '#Kabobs-SubMenu' );
+KabobsMenuItem.addEventListener( 'mouseover', ( ) => {
+  WidgetsSubMenuGroup.classList.remove( 'showSubMenu' );
+  onSubMenu = true;
+  enterMenuPose( );
+  KabobsSubMenuGroup.classList.add( 'showSubMenu' );
+} );
+KabobsMenuItem.addEventListener( 'mouseout', ( ) => {
+  if ( !onSubMenu ) {
+    leavePose( );
+    KabobsSubMenuGroup.classList.remove( 'showSubMenu' );
+  }
+} );
+
+// Listener to check if the mouse is outside the Menu Area.
+const outsideMenuArea = document.querySelector( '#canvas-size' );
+outsideMenuArea.addEventListener( 'mouseover', ( ) => {
+  resetToNewPose( );
 } );
